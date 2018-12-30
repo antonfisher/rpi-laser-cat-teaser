8b706c67c5374f3a9201a4e36e31625d078f12de
Anton Fisher <a.fschr@gmail.com>
2018-12-30 15:45:43 -0800

    fix: rename image to editor

--- pkg/editor/editor.go
+++ pkg/editor/editor.go
@@ -1,4 +1,4 @@
-package image
+package editor
 
 import (
 	"bytes"
@@ -38,6 +38,7 @@ func (e *Editor) JpegBytes(quality int) []byte {
 // DrawCrosshead on image
 func (e *Editor) DrawCrosshead(x, y, crossheadSize, crossheadStrokeWidth int) {
 	imageSize := e.Image.Bounds().Size()
+	cBlue := &color.RGBA{0, 0, 255, 255}
 
 	for i := 0; i < crossheadSize; i++ {
 		if i > crossheadSize/3 && i < crossheadSize*2/3 {
@@ -46,14 +47,59 @@ func (e *Editor) DrawCrosshead(x, y, crossheadSize, crossheadStrokeWidth int) {
 		for w := -crossheadStrokeWidth / 2; w < crossheadStrokeWidth/2; w++ {
 			xd := x - crossheadSize/2 + i
 			if xd >= 0 && xd < imageSize.X && y >= 0 && y < imageSize.Y {
-				e.Image.Set(xd, y+w, &color.RGBA{255, 0, 0, 255})
+				e.Image.Set(xd, y+w, cBlue)
 			}
 			yd := y - crossheadSize/2 + i
 			if x >= 0 && x < imageSize.X && yd >= 0 && yd < imageSize.Y {
-				e.Image.Set(x+w, yd, &color.RGBA{255, 0, 0, 155})
+				e.Image.Set(x+w, yd, cBlue)
 			}
 		}
 	}
+}
+
+// DiffGreen diff with another image based green channel
+func (e *Editor) DiffGreen(img image.Image, threshold uint32) [][]int {
+	cDiff := &color.RGBA{255, 255, 0, 255}
+	w := e.Width()
+	h := e.Height()
+
+	diffArray := make([][]int, w)
+	for i := range diffArray {
+		diffArray[i] = make([]int, h)
+	}
+
+	for x := 0; x < w; x++ {
+		for y := 0; y < h; y++ {
+			_, g1, _, _ := e.Image.At(x, y).RGBA()
+			_, g2, _, _ := img.At(x, y).RGBA()
+			if colorDiff(g1, g2) > threshold {
+				e.Image.Set(x, y, cDiff)
+				diffArray[x][y] = 1
+			}
+		}
+	}
+
+	return diffArray
+}
+
+// Clone creates a of currently edited image
+func (e *Editor) Clone() image.Image {
+	size := e.Image.Bounds().Size()
+
+	// create empty image with the same size (to be able to draw)
+	newImage := image.NewRGBA(image.Rect(0, 0, size.X, size.Y))
+
+	// copy source image to the new one
+	draw.Draw(newImage, e.Image.Bounds(), e.Image, image.ZP, draw.Src)
+
+	return newImage
+}
+
+func colorDiff(a, b uint32) uint32 {
+	if a > b {
+		return a - b
+	}
+	return b - a
 }
 
 // NewEditorFromJpegBytes creates new jpeg image from []byte
